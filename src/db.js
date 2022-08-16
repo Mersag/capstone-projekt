0e5c1577a73982895fb329c2965fb71616d6b94d
Meral Sager <mersa22@gmx.de>
2022-08-16 14:06:04 +0200

    add component db.js

---
+++ src/db.js
@@ -0,0 +1,20 @@
+export const db = [
+	{
+		id: 0,
+		name: 'cobra',
+		sanskritName: 'bhujaṅgāsana',
+		effects: ['körperlich', 'geistig', 'energetisch', 'chakra'],
+	},
+	{
+		id: 1,
+		name: 'Schulterstand',
+		sanskritName: 'pügpohütphoür',
+		effects: ['körperlich', 'geistig', 'energetisch', 'chakra'],
+	},
+	{
+		id: 3,
+		name: 'Krähe',
+		sanskritName: 'hjshfjsh',
+		effects: ['körperlich', 'geistig', 'energetisch', 'chakra'],
+	},
+];
