5d543c03c603170a0b425d71546d009487c37adf
Meral Sager <mersa22@gmx.de>
2022-08-31 15:05:56 +0200

    add app name h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 const StyledHeader = styled.header`
 	padding: 30px;
 	background-color: #83c5be;
+	color: white;
 `;
 
 export default function Header() {
 	return (
 		<StyledHeader>
-			{/*<span style="font-size: 32px; margin: 21px 0;">My Yoga Routine</span>*/}
+			<h2>My Yoga Routine</h2>
 		</StyledHeader>
 	);
 }
