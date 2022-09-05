c80bbb5367a2f010e06f327a68be6d300d726cd2
Meral Sager <mersa22@gmx.de>
2022-09-05 10:28:28 +0200

    header fixed add css tool reset to globalstyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
+	position: fixed;
+	z-index: 100;
+	width: 100%;
 	padding: 20px;
 	background-color: #83c5be;
 	color: white;
